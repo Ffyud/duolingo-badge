8eea365d2e70663e618022f121e0fecef053963e
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-09-30 21:11:12 +0200

    remove export

--- duolingo-badge.js
+++ duolingo-badge.js
@@ -1,6 +1,6 @@
 import { LitElement, html} from 'lit-element';
 
-export class DuolingoBadge extends LitElement {
+class DuolingoBadge extends LitElement {
   render() {
     return html`
       <div>Vet hoge score</div>
