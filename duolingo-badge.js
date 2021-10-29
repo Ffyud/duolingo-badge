3bd457f94a713ba44c8a889cf05ec829800801d9
Ffyud <5263778+Ffyud@users.noreply.github.com>
2021-10-29 16:21:05 +0200

    uitbreiden

--- duolingo-badge.js
+++ duolingo-badge.js
@@ -1,11 +1,19 @@
-import { LitElement, html} from 'lit-element';
+import {html, css, LitElement} from 'lit';
 
 export class DuolingoBadge extends LitElement {
+  static styles = css`p { color: blue }`;
+
+  static properties = {
+    name: {type: String},
+  };
+
+  constructor() {
+    super();
+    this.name = 'Duolingo';
+  }
+
   render() {
-    return html`
-      <div>Vet hoge score</div>
-    `;
+    return html`<p>Hello, ${this.name}!</p>`;
   }
 }
-
 customElements.define('duolingo-badge', DuolingoBadge);
